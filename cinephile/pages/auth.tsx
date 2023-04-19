5342a0e2436849e1310dbf18cd33a58323862faa
Bora <borabalos9@gmail.com>
2023-04-19 18:57:30 +0200

    auth interface done

--- cinephile/pages/auth.tsx
+++ cinephile/pages/auth.tsx
@@ -1,14 +1,45 @@
+import { use, useCallback, useState } from "react";
+import Input from "../components/input";
+
 const Auth = () => {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const [variant, setVariant] = useState('login');
+
+    const toggleVariant = useCallback(() => {
+        setVariant((currentVariant) => currentVariant === 'login' ? 'register' : 'login')
+    }, [])
+
     return (
         <div className="relative h-full w-full bg-[url('/images/hero-2.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
-            <nav className="px-12 py-5">
-                <img src="/images/logo_red.svg" alt="Logo" className="h-12" />
-            </nav>
-            <div className="flex justify-center">
-                <div className="bg-black bg-opacity-70 px-16 py-16 self-center mt-2 lg:w-2/5 lg:max-w-md rounded-md w-full">
-                    <h2 className="text-white text-4xl mb-8 font-semibold">
-                        Sign in
-                    </h2>
+            <div className="bg-black w-full h-full lg:bg-opacity-0">
+                <nav className="px-12 py-5">
+                    <img src="/images/logo_red.svg" alt="Logo" className="h-12" />
+                </nav>
+                <div className="flex justify-center">
+                    <div className="bg-black bg-opacity-70 px-16 py-16 self-center mt-2 lg:w-2/5 lg:max-w-md rounded-2xl w-full">
+                        <h2 className="text-white text-4xl mb-8 font-semibold">
+                            {variant === 'login' ? 'Sign in' : 'Sign Up'}
+                        </h2>
+                        <div className="flex flex-col gap-4">
+                            {variant === 'register' && (
+                                <Input label="Username" onChange={(event: any) => setName(event.target.value)} id="name" value={name}/>
+                            )}
+                            <Input label="Email" onChange={(event: any) => setEmail(event.target.value)} id="email" type="email" value={email}/>
+                            <Input label="Password" onChange={(event: any) => setPassword(event.target.value)} id="password" type="password" value={password}/>
+                        </div>
+                        <button className="bg-red py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition">
+                            {variant === 'login' ? 'Login' : 'Sign Up' }
+                        </button>
+                        <p className="text-neutral-500 mt-12">
+                            {variant === 'login' ? 'First time here?' : 'Already have an account?'}
+                            <span onClick={toggleVariant} className="text-white ml-1 hover:underline cursor-pointer">
+                                {variant === 'login' ? 'Create an account' : 'Login'}
+                            </span>
+                        </p>
+                    </div>
                 </div>
             </div>
         </div>
