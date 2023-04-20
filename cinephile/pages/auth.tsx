2c737216683d912076510dae47e19a0923a74f8e
Bora <borabalos9@gmail.com>
2023-04-21 00:02:43 +0200

    OAuths added and authentication finished

--- cinephile/pages/auth.tsx
+++ cinephile/pages/auth.tsx
@@ -4,6 +4,9 @@ import Input from "../components/input";
 import { signIn } from 'next-auth/react';
 import { useRouter } from "next/router";
 
+import { FcGoogle } from 'react-icons/fc';
+import { FaGithub } from 'react-icons/fa';
+
 const Auth = () => {
     const router = useRouter();
 
@@ -28,7 +31,7 @@ const Auth = () => {
             console.log(error);
         }
     }, [email, password, router]);
-    
+
     const register = useCallback(async () => {
         try {
             await axios.post('/api/register', {
@@ -64,6 +67,14 @@ const Auth = () => {
                         <button onClick={variant === 'login' ? login : register} className="bg-red py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition">
                             {variant === 'login' ? 'Login' : 'Sign Up' }
                         </button>
+                        <div className="flex flex-row items-center gap-4 mt-8 justify-center">
+                            <div onClick={() => signIn('google', { callbackUrl: '/' })} className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition">
+                                <FcGoogle size={30} />
+                            </div>
+                            <div onClick={() => signIn('github', { callbackUrl: '/' })} className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition">
+                                <FaGithub size={30} />
+                            </div>
+                        </div>
                         <p className="text-neutral-500 mt-12">
                             {variant === 'login' ? 'First time here?' : 'Already have an account?'}
                             <span onClick={toggleVariant} className="text-white ml-1 hover:underline cursor-pointer">
