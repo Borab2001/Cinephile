b79181258748964e5fa829234671406351e36a36
Bora <borabalos9@gmail.com>
2023-04-21 19:49:49 +0200

    Routes protections

--- cinephile/pages/index.tsx
+++ cinephile/pages/index.tsx
@@ -1,7 +1,33 @@
+import { NextPageContext } from "next";
+import { getSession, signOut } from "next-auth/react";
+
+import useCurrentUser from "@/hooks/useCurrentUser";
+
+export async function getServerSideProps(context: NextPageContext) {
+  const session = await getSession(context);
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: '/auth',
+        permanent: false,
+      }
+    }
+  }
+
+  return {
+    props: {}
+  }
+}
+
 export default function Home() {
+  const { data: user } = useCurrentUser();
+
   return (
     <>
-    <h1 className="text-2xl text-red">Hello World</h1>
+      <h1 className="text-2xl text-red">Hello World</h1>
+      <p className="text-white">Logged in as : {user?.email}</p>
+      <button className="h-10 w-full bg-white" onClick={() => signOut()}>Logout</button>
     </>
   )
 }
