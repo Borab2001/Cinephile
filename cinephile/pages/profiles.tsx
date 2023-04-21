b0a9ed8f5718383dfe38f0eda09884e6950adb6e
Bora <borabalos9@gmail.com>
2023-04-21 20:08:16 +0200

    profile beginning

--- cinephile/pages/profiles.tsx
+++ cinephile/pages/profiles.tsx
@@ -1,1 +1,9 @@
-const Profiles
+const Profiles = () => {
+    return (
+        <div>
+            <p className="text-white text-4xl">Profiles</p>
+        </div>
+    )
+};
+
+export default Profiles;
