4b03a9842f87c547198045cac0eab292f98b08a1
Bora <borabalos9@gmail.com>
2023-06-01 16:12:05 +0200

    Handle DELETE request for favoriting

--- cinephile/pages/api/favorite.ts
+++ cinephile/pages/api/favorite.ts
@@ -34,6 +34,38 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 
             return res.status(200).json(user);
         }
+
+        if (req.method === 'DELETE') {
+            const { currentUser } = await serverAuth(req);
+
+            const { movieId } = req.body;
+
+            const existingMovie = await prismadb.movie.findUnique({
+                where: {
+                    id: movieId,
+                }
+            });
+
+            if (!existingMovie) {
+                throw new Error('Invalid ID');
+            }
+
+            const updatedFavoriteIds = without(currentUser.favoriteIds, movieId);
+
+            const updatedUser = await prismadb.user.update({
+                where: {
+                    email: currentUser.email || '',
+                },
+                data: {
+                    favoriteIds: updatedFavoriteIds,
+                }
+            });
+
+            return res.status(200).json(updatedUser);
+        }
+
+        return res.status(405).end();
+        
     } catch (error) {
         console.log(error);
         return res.status(400).end();
