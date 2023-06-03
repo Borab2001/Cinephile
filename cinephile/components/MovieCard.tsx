1713e8fe0b4aa989d188cb3a39ce4d656bcb13fa
Bora <borabalos9@gmail.com>
2023-06-04 01:33:46 +0200

    Router added to MovieCard

--- cinephile/components/MovieCard.tsx
+++ cinephile/components/MovieCard.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useRouter } from "next/router";
 
 import { BsFillPlayFill } from 'react-icons/bs';
 import FavoriteButton from "./FavoriteButton";
@@ -10,6 +11,8 @@ interface MovieCardProps {
 const MovieCard: React.FC<MovieCardProps> = ({
     data
 }) => {
+    const router = useRouter();
+
     return (
         <div className="group bg-zinc-900 col-span relative h-[12vw]">
             <img
@@ -61,7 +64,7 @@ const MovieCard: React.FC<MovieCardProps> = ({
                                 transition 
                                 hover:bg-neutral-300
                             " 
-                            onClick={() => {}}>
+                            onClick={() => router.push(`/watch/${data?.id}`)}>
                                 <BsFillPlayFill size={30} />
                          </div>
                          <FavoriteButton movieId={data?.id} />
