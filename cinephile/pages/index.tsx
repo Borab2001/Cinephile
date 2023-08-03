730e96290ea39c4700eaee362f7852c875954fe1
Bora <borabalos9@gmail.com>
2023-08-03 18:33:23 +1000

    Changed the place of analytics

--- cinephile/pages/index.tsx
+++ cinephile/pages/index.tsx
@@ -1,6 +1,5 @@
 import { NextPageContext } from "next";
 import { getSession } from "next-auth/react";
-import { Analytics } from '@vercel/analytics/react';
 
 import Navbar from "@/components/Navbar";
 import Billboard from "@/components/Billboard";
@@ -34,7 +33,6 @@ export default function Home() {
 
   return (
     <>
-      <Analytics />
       <InfoModal visible={isOpen} onClose={closeModal} />
       <Navbar />
       <Billboard />
